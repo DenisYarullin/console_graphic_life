76a9e30085a902cb7d476236d0bbf5f5b051a57a
Punisher <dryarullin@gmail.com>
2016-03-08 14:14:40 +0500

    2

--- life.cpp
+++ life.cpp
@@ -1,1 +1,29 @@
+#include "life.h"
+#include <random>
+#include <algorithm>
 
+
+
+Life::Life(unsigned int weight_, unsigned int height_)
+    : weight(weight_), height(height_)
+{
+    world.resize(weight);
+    for(auto &x: world)
+        x.resize(height);
+}
+
+
+void Life::init_world()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 10000);
+
+    for(auto &h: world)
+    {
+        for(auto &w: h)
+        {
+            w.is_live = dis(gen) % 2;
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include "life.h"
 
 using namespace std;
 
 int main()
 {
+    Life life(10, 10);
+
     cout << "Hello World!" << endl;
     return 0;
 }
-
